6c12886e9276b18c526dc7ece1075149bf722705
snairkicks <info@snairkicks.com>
2022-01-27 11:19:31 -0800

    Corrections
    stakingwallet uint was empty so I added the variable 8. Also changed the name of 1st contract to "If Statements" because thats what it is focused on.

--- solidity-plygrd.sol
+++ solidity-plygrd.sol
@@ -1,6 +1,6 @@
 pragma solidity ^ 0.8.0;
 
-contract UdemyStuff{
+contract ifStatements{
     uint orange = 8;
     function validateOranges() public view returns(bool){
         if(orange == 5){
@@ -8,13 +8,16 @@ contract UdemyStuff{
             } else {
             return false;}
 } 
-    uint stakingWallet;
+    uint stakingWallet = 8;
     function airDrop() public view returns(uint){
-        if(stakingWallet >= 10){
+        if(stakingWallet >= 7){
             return stakingWallet + 10;
         } else {
             return stakingWallet + 1;
         }
     }
+}
+contract arrays{
+    
 }
 ;
