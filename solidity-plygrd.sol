a6cd675b8b2745d7f5bdc2081b7e421eb7e446d0
snairkicks <info@snairkicks.com>
2022-01-27 12:01:05 -0800

    Taking Notes
    Adding notes to scope section.

--- solidity-plygrd.sol
+++ solidity-plygrd.sol
@@ -1,5 +1,45 @@
 pragma solidity ^ 0.8.0;
 
+
+contract Scopes{
+    /* Variable Scope - Functions & Variables
+    Keywords
+    private = you can call the function inside the contract.
+    
+    public = you can call the function from outside the smart contract as well inside the smart contract.
+    
+    internal = called ONLY within the contract OR other contracts that inherit
+    the smart contract internal is slightly less restrictive than public.
+    
+    external = you can ONLY call the function outside the contract not from other functions within
+
+    // c++ also has inheritance 
+
+    *you can modify the rules of scope with those key words
+    RULE OF THUMB: Give the minimum amount of priviledge to any entity.
+
+    State Variables vs Local Variables 
+
+    */
+    
+    uint public data = 10; // state variable 
+    
+    function x() external pure returns (uint) {
+        uint data = 25; // local variable overides state. it remains in the function.
+        return data;
+    }
+    
+    function l() public view returns(uint) {
+        uint newData = data - 3;
+        return newData;
+    }
+    
+    
+    function y() public view returns (uint) {
+        return data;
+    }
+
+}
 contract ifStatements{
     uint orange = 8;
     function validateOranges() public view returns(bool){
