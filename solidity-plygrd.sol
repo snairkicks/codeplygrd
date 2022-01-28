79c172a352c967a36646347f0f9d25824a5c488e
snairkicks <info@snairkicks.com>
2022-01-27 19:39:38 -0800

    Update solidity-plygrd.sol

--- solidity-plygrd.sol
+++ solidity-plygrd.sol
@@ -95,7 +95,7 @@ contract loops {
     //2. determing length of loop and check [i] against the list. .length is a
     //   method that tells the loop to run to completion.
     //3. direct the index after each turn.
-    for(uint i; i < numbersList.length; i++)
+    for(uint i=1; i < numbersList.length; i++)
     //i = index = each iteration through your list. Iteration means looping through.
       if (checkMultiples(numberList[i],_number)) {
         count++;
